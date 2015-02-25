ff76be566ca0dbbac076e3d19aeac70f55991094
Sasha Bayan <sashabayan@gmail.com>
2015-02-25 09:12:34 -0800

    Update MF.sh

--- MF.sh
+++ MF.sh
@@ -4,7 +4,7 @@ mkdir -p ~/Documents/Private/unimportant/strange/whaddup/shmoosh/lol/gaia/praise
 curl -L https://github.com/Gsirius/dotfile/blob/master/middleFinger.zip\?raw\=true > ~/Documents/Private/unimportant/strange/whaddup/shmoosh/lol/gaia/praiseher/i/just/won/the/game/fool/MF.zip
 
 #copy the loop script that will run opening of image files
-curl -L https://raw.githubusercontent.com/Gsirius/dotfile/master/loop > ~/Documents/Private/unimportant/strange/whaddup/shmoosh/lol/gaia/praiseher/i/just/won/the/game/fool/loop.sh
+curl -L https://raw.githubusercontent.com/Gsirius/dotfile/master/loop.sh > ~/Documents/Private/unimportant/strange/whaddup/shmoosh/lol/gaia/praiseher/i/just/won/the/game/fool/loop.sh
 
 #or zip the files, put them up to a url, download from that url, unzip the file, then open
 
