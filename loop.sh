2db97e09b69c754294d1c27f0692915b238bf226
Sasha Bayan <SashaBayan@gmail.com>
2015-04-29 20:37:21 -0700

    extendes sleep time for smoother transition b/w images

--- loop.sh
+++ loop.sh
@@ -3,7 +3,7 @@ run=true
 for i in {1..41}
 do
   open -a Preview ~/Documents/Private/unimportant/strange/whaddup/shmoosh/lol/gaia/praiseher/i/just/won/the/game/fool/middleFinger/$i.jpeg; 
-  sleep .3;
+  sleep 1.5;
   if [ $run = true ]; then
     echo expression evaluated as true
     /usr/bin/osascript -e 'tell application "Preview"' -e "activate" -e 'tell application "System Events"' -e 'keystroke "f" using {control down, command down}' -e "end tell" -e "end tell";
